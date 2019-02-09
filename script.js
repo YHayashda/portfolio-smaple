40fd349f7f34dc778fe5c9ca1281cd9a9ccf9e44
yoshi <yoshiaki._1229@ezweb.ne.jp>
2019-02-09 13:12:23 +0900

    adding scroll event

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ $(document).ready(function() {
   $("#minus-btn").click(function() {
     $("body").css('font-size', '-=4');
   });
+
+
+  // スクロールイベント
+  $(window).scroll(function() {
+    let scrl = $(window).scrollTop();
+    const about = $(".about").offset().top;
+    if (scrl > about*0.3) {
+      $(".about").css({
+        opacity:"1",
+        transform: "translateY(0)"
+      });
+    }
+  });
 });
