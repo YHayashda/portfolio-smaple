3cfb12863173afb348a402fd635b067864f781b8
yoshi <yoshiaki._1229@ezweb.ne.jp>
2019-02-11 14:54:25 +0900

    modifying colorDesign

--- script.js
+++ script.js
@@ -16,8 +16,36 @@ $(document).ready(function() {
   $(window).scroll(function() {
     let scrl = $(window).scrollTop();
     const about = $(".about").offset().top;
-    if (scrl > about*0.3) {
+    if (scrl > about*0.25) {
       $(".about").css({
+        opacity:"1",
+        transform: "translateY(0)"
+      });
+    }
+    const works = $(".Works").offset().top;
+    if (scrl > works*0.5) {
+      $(".Works").css({
+        opacity:"1",
+        transform: "translateY(0)"
+      });
+    }
+    const education = $(".Education").offset().top;
+    if (scrl > education*0.6) {
+      $(".Education").css({
+        opacity:"1",
+        transform: "translateY(0)"
+      });
+    }
+    const Experience = $(".Experience").offset().top;
+    if (scrl > Experience*0.8) {
+      $(".Experience").css({
+        opacity:"1",
+        transform: "translateY(0)"
+      });
+    }
+    const contact = $(".contact").offset().top;
+    if (scrl > contact*0.8) {
+      $(".contact").css({
         opacity:"1",
         transform: "translateY(0)"
       });
